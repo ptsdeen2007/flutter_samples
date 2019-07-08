4a5d4e6a87b0c46a10d7254def3db35b4bba0a2c
Admin <ptsdeen2007@gmail.com>
2019-07-08 12:49:28 +0530

    first

--- lib/main.dart
+++ lib/main.dart
@@ -8,11 +8,11 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Demo Home Page'),
     );
   }
 }
