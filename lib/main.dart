5a057f9834d94a85c7fd3558a3a0e11882eaec9f
Admin <ptsdeen2007@gmail.com>
2019-07-08 15:37:01 +0530

    top bar

--- lib/main.dart
+++ lib/main.dart
@@ -17,6 +17,14 @@ class MyApp extends StatelessWidget {
   }
 }
 
+class Choice {
+  const Choice({this.title, this.icon});
+
+  final String title;
+  final IconData icon;
+}
+
+
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
 
@@ -27,39 +35,70 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
+    var accentColor2 = Theme.of(context).accentColor;
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+        body: ListView(
+      children: <Widget>[
+        Container(
+          color: accentColor2,
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              children: <Widget>[
+                Icon(Icons.menu, color: Colors.white),
+                Flexible(
+                  fit: FlexFit.tight,
+                  child: Container(
+                    child: Padding(
+                      padding: const EdgeInsets.only(right: 8.0, left: 8.0),
+                      child: Column(
+                        children: <Widget>[
+                          Align(
+                            child: Text(
+                              "Your location",
+                              style: TextStyle(
+                                color: Colors.white,
+                              ),
+                              textAlign: TextAlign.left,
+                            ),
+                            alignment: Alignment.centerLeft,
+                          ),
+                          Text(
+                            "This text is very very very very very very very very very very very very very very very very very very very very very very very very very long",
+                            overflow: TextOverflow.ellipsis,
+                            maxLines: 1,
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+                Icon(
+                  Icons.edit,
+                  color: Colors.white,
+                  size: 15,
+                ),
+                SizedBox(
+                  height: 1,
+                  width: 8,
+                ),
+                Icon(Icons.pin_drop, color: Colors.white),
+              ],
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+          ),
+        )
+      ],
+    )
+        // This trailing comma makes auto-formatting nicer for build methods.
+        );
+  }
+
+  void doSumthing() {
+    print("hello");
   }
 }
